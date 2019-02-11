0cd8f7048b0048e698c64ef157015c50fbcaec26
Russell Homan <drhoman99@gmail.com>
2019-02-11 16:42:36 +0000

    adding generator project

---
+++ script.js
@@ -0,0 +1,21 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background;
+}
+
+css.textContent = "linear-gradient(to right, #00ff00 ,#ff0000)";
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
